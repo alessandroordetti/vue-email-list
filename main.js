9f635053148f9b768c039a4d31280af00942f1a9
alessandroordetti <alessandro.ordetti@gmail.com>
2022-03-24 17:41:32 +0100

    putting self outsite for loop

--- main.js
+++ main.js
@@ -6,15 +6,16 @@ const app = new Vue ({
     methods: {},
     created(){},
     mounted(){
+        const self = this;
         for (let i = 0; i < 10; i++) {
-            const self = this;
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then(function(response){
             const result = response.data.response;
             self.risultato.push(result);
-            console.log(self.risultato);
             });
         };
+
+        console.log(self.risultato);
     },
 })
 
